a1de68ac151f06474da7d3c7558e813a3669ef55
EliasSarges <edjcss@outlook.com>
2021-08-11 14:23:06 -0300

    refactor(sign-up-controller): move duplicated return error for a helper badRequest

--- src/presentation/controllers/sign-up-controller.ts
+++ src/presentation/controllers/sign-up-controller.ts
@@ -1,26 +1,16 @@
 import { HttpRequest, HttpResponse } from './protocols/http'
 import { MissingParameterError } from './errors/missing-param-errors'
+import { badRequest } from './helpers/http-helper'
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParameterError('name')
-      }
+      return badRequest(new MissingParameterError('name'))
     }
 
     if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParameterError('email')
-      }
+      return badRequest(new MissingParameterError('email'))
     }
 
-    const response: HttpResponse = {
-      statusCode: 400,
-      body: {}
-    }
-
-    return response
+    return badRequest(new Error('body is not provided'))
   }
 }
