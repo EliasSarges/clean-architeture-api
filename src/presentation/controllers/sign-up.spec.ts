6eaacef87c5cbb20acb4c81b1290a7497be7b93d
EliasSarges <edjcss@outlook.com>
2021-11-07 19:16:15 -0300

    chore: add jest configs and scripts

--- src/presentation/controllers/sign-up.spec.ts
+++ src/presentation/controllers/sign-up.spec.ts
@@ -64,7 +64,7 @@ describe('Sign Up controller', () => {
       }
     }
 
-    const httpResponse = await await sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse?.statusCode).toBe(400)
     expect(httpResponse?.body).toEqual(new MissingParameterError('name'))
   })
